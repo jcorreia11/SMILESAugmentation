02857c18254fde021ce9958a316ad5ccbd21be11
João Correia <jfscorreia95@gmail.com>
2022-06-05 15:30:06 +0100

    [ADD] PermutationsReactionSmilesEnumerator implementation

--- src/smiles_augmentation/reaction_smiles_enumerator.py
+++ src/smiles_augmentation/reaction_smiles_enumerator.py
@@ -76,6 +76,8 @@ class ReactionSmilesEnumerator:
         List[str]:
             List of reaction SMILES strings.
         """
+        if seed:
+            random.seed(seed)
         enumerated_reactants, enumerated_products = _enumerate_reactants_products(reaction_smiles,
                                                                                   n_max,
                                                                                   remove_duplicates,
@@ -225,6 +227,122 @@ class LevenshteinReactionSmilesEnumerator:
         # if reaction smiles has reactants>>products
         else:
             return list(map('>>'.join, pairs))
+
+    def enumerate(self, n_max: int = 1) -> List[str]:
+        """
+        Enumerate `n_max` new reaction SMILES strings from the given reaction SMILES using the Levenshtein distance.
+
+        Parameters
+        ----------
+        n_max: int
+            Maximum number of reaction SMILES to enumerate.
+
+        Returns
+        -------
+        List[str]:
+            List of reaction SMILES strings.
+        """
+        if isinstance(self.reaction_smiles, str):
+            return self._enumerate_one(self.reaction_smiles, n_max, self.remove_duplicates, self.seed, self.verbose)
+        else:
+            return Parallel(n_jobs=self.n_jobs,
+                            backend="multiprocessing")(delayed(self._enumerate_one)(reaction_smiles,
+                                                                                    n_max,
+                                                                                    self.remove_duplicates,
+                                                                                    self.seed,
+                                                                                    self.verbose)
+                                                       for reaction_smiles in self.reaction_smiles)
+
+
+class PermutationsReactionSmilesEnumerator:
+    """
+    Class for enumerating reaction SMILES using permutations.
+    """
+
+    def __init__(self,
+                 reaction_smiles: Union[str, List[str]],
+                 remove_duplicates: bool = True,
+                 seed: int = None,
+                 n_jobs: int = 1,
+                 verbose: int = 0):
+        """
+        Initializes a PermutationsReactionEnumerator object.
+
+        Parameters
+        ----------
+        reaction_smiles: Union[str, List[str]]
+            Reaction SMILES string or list of reaction SMILES strings to permutate.
+        remove_duplicates: bool
+            Whether to remove duplicates from the enumerated reaction SMILES.
+        seed: int
+            Random seed for reproducibility.
+        n_jobs: int
+            Number of parallel jobs to run.
+        verbose: int
+            Verbosity level.
+                0: No RDKit outputs.
+                1: Show Warnings and errors.
+        """
+        self.reaction_smiles = reaction_smiles
+        self.remove_duplicates = remove_duplicates
+        self.seed = seed
+        self.n_jobs = n_jobs
+        self.verbose = verbose
+        if self.verbose == 0:
+            RDLogger.DisableLog("rdApp.*")
+
+    @staticmethod
+    def _enumerate_one(reaction_smiles,
+                       n_max: int = 1,
+                       remove_duplicates: bool = True,
+                       seed: int = None,
+                       verbose: int = 0) -> List[str]:
+        """
+        Enumerate new reaction SMILES string from the given `reaction_smiles` string
+        using permutations.
+
+        Parameters
+        ----------
+        reaction_smiles: str
+            Reaction SMILES string to enumerate.
+        n_max: int
+            Maximum number of SMILES to enumerate in the SmilesEnumerator calls.
+        remove_duplicates: bool
+            Remove duplicates from the enumerated SMILES in the SmilesEnumerator calls.
+        seed: int
+            Random seed.
+        verbose: int
+            Verbosity level.
+                0: No RDKit outputs.
+                1: Show Warnings and errors.
+
+        Returns
+        -------
+        List[str]:
+            List of reaction SMILES strings.
+        """
+        if seed:
+            random.seed(seed)
+        reactants = reaction_smiles.split(">")[0]
+        products = reaction_smiles.split(">")[-1]
+
+        permutated_reactants = list(itertools.permutations(reactants.split(".")))
+        permutated_reactants = ['.'.join(reagent) for reagent in permutated_reactants]
+        permutated_products = list(itertools.permutations(products.split(".")))
+        permutated_products = ['.'.join(reagent) for reagent in permutated_products]
+
+        results = list(itertools.product(permutated_reactants, permutated_products))
+        if remove_duplicates:
+            results = list(set(results))
+        if len(results) > n_max:
+            results = random.sample(results, n_max)
+        # if reaction smiles has reactants>reagents>products
+        if len(reaction_smiles.split('>>')) == 1:
+            reagents = reaction_smiles.split(">")[1]
+            return [f"{r_i[0]}>{reagents}>{r_i[1]}" for r_i in results]
+        # if reaction smiles has reactants>>products
+        else:
+            return list(map('>>'.join, results))
 
     def enumerate(self, n_max: int = 1) -> List[str]:
         """
